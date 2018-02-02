5db3318bc68a443a28e28c234c33c1781a5cc5a3
yungz7 <wyungz7@gmail.com>
2018-02-02 08:31:00 +0800

    add test controller and views

---
+++ backend/controllers/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace backend\controllers;
+
+use yii\web\Controller;
+
+class TestController extends Controller{
+
+	public function actionIndex(){
+		return $this->render('index');
+	}
+}
